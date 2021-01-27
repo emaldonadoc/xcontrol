bb39b93ec83594d8ea0609d2f02ee3aef06bfc2d
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-01-27 15:42:59 -0600

    Refactoring gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,15 +2,24 @@ const gulp = require('gulp');
 const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 
-gulp.task('clean', () => 
-  gulp.src('dist/', {allowEmpty: true, read: false})
-    .pipe(clean())
-);
 
-gulp.task('build', () => 
+const cleanUp = () => 
+	gulp
+	.src('dist/', { allowEmpty: true, read: false })
+	.pipe(clean());
+	
+const building = () => 
 	gulp.src('src/**/*.js')
 		.pipe(babel({
 			plugins: ['@babel/transform-runtime']
     }))
 		.pipe(gulp.dest('dist'))
-);
+
+
+gulp.task('watch', () => {
+	return gulp.watch('src/**/*.js', gulp.series(cleanUp, building))
+});
+
+gulp.task('clean', cleanUp);
+
+gulp.task('build', gulp.series(cleanUp, building));
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,10 @@ var http = require('http');
 const hostname = '127.0.0.1';
 const port =  6660
 
+const paths = () => {
+  console.log('creating listening paths');
+}
+
 export const startServer = () => {
   const server = http.createServer();
 
