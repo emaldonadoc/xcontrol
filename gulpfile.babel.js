2b9294a6fcfa86720dcffa51da31556d66b64494
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-01-27 15:18:01 -0600

    Add building gulp taks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,15 @@ const gulp = require('gulp');
 var clean = require('gulp-clean');
 const babel = require('gulp-babel');
 
-gulp.task('default', () =>
+gulp.task('clean', () => 
+  gulp.src('dist/', {allowEmpty: true, read: false})
+    .pipe(clean())
+);
+
+gulp.task('build', () => 
 	gulp.src('src/**/*.js')
 		.pipe(babel({
 			plugins: ['@babel/transform-runtime']
     }))
-    .pipe(clean())
 		.pipe(gulp.dest('dist'))
 );
