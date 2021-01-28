86d609999007c24c07c125084da2bab445be8ade
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-01-28 16:34:51 -0600

    Add spec on load controllers and start server

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -1,7 +1,34 @@
+import http from 'http';
+import controllers from 'app/controllers';
+import middleware from 'app/server/middleware';
 
+import { startServer } from 'app/server'
+
+
+const listenFn = jest.fn();
+
+jest.mock('http');
+
+jest.mock('app/controllers', () => ({
+  default: { path: 'default', handler: () => { } }
+}));
+
+jest.mock('app/server/middleware', () => ({
+  registerController: jest.fn(),
+  build: jest.fn()
+}));
 
 describe('Start server spec', () => {
+
   it('Should register controllers', () => {
 
+    http.createServer.mockReturnValue({ listen: listenFn })
+
+    startServer();
+
+    expect(middleware.registerController).toHaveBeenCalled();
+    expect(middleware.build).toHaveBeenCalled();
+    expect(http.createServer).toHaveBeenCalled();
+    expect(listenFn).toHaveBeenCalled();
   });
 });
