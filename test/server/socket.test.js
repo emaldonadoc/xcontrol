3b834fcccc21756bf7528644afa172f6b212144c
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-04-30 12:01:18 -0500

    Add socket expecting

--- test/server/socket.test.js
+++ test/server/socket.test.js
@@ -1,7 +1,26 @@
+import io from 'socket.io';
+import { initializeSocket } from 'app/server/socket';
+
+jest.mock('socket.io');
 
 describe('socket', () => {
 
   it('should create socket attaching http server', () => {
+
+    const ws = {
+      on: jest.fn(),
+    };
+    const of = jest.fn().mockReturnValue(ws);
+    const socket = { of };
+    const http = {};
+
+    io.mockReturnValue(socket);
+
+    initializeSocket(http)
+
+    expect(io).toHaveBeenCalledWith(http);
+    expect(of).toHaveBeenCalledWith('/ws');
+    expect(ws.on).toHaveBeenCalledWith('connection',  expect.any(Function));
 
   });
 });
