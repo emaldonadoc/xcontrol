c4c29bf8563c103a855f4fb4be34463c392c650e
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-05-03 11:44:54 -0500

    Run command as child process

--- src/utils/checkSO.js
+++ src/utils/checkSO.js
@@ -1,11 +1,13 @@
-import lockWindowsApi from 'lock-your-windows';
+const { exec } = require('child_process');
 
-export const isWin = (platform) => {    
-    if(platform !== 'win32'){
+export const isWin = (platform) => {
+    if (platform !== 'win32') {
         throw new ErrorEvent('It can only use windows')
     }
 }
 
 export const isLock = () => {
-    return lockWindowsApi.isLocked()
+    exec('tasklist | find "LogonUI.exe"', (error, stdout, stderr) => {
+        console.log({ stdout, stderr, error })
+    })
 }
