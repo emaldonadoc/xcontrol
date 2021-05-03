741f99c0e5d0a96354a86988a8519fc8b9bcd362
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2021-05-03 15:23:30 -0500

    Move and expose to hostService folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _server = require("./server");
-
-var _socket = require("./server/socket");
-
-console.log('initialize control application');
-(0, _server.startServer)();
-(0, _socket.initializeSocket)();
